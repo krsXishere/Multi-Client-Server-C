650885e113e9b2cf5de78cd471da22d281217a04
KrsXishere <lxghozt28@gmail.com>
2024-12-13 22:49:48 +0700

    add: adding nvidia ai

--- header/server.h
+++ header/server.h
@@ -1,44 +1,21 @@
-// //
-// // Created by Krisna Purnama on 05/12/24.
-// //
 //
-// #ifndef SERVER_H
-// #define SERVER_H
+// Created by Krisna Purnama on 05/12/24.
 //
-// #include <netinet/in.h>
-//
-// #define PORT 8080
-// #define BACKLOG 10
-// #define MAX_CLIENTS 30
-//
-//
-// // Struktur untuk server
-// typedef struct {
-//     int server_fd;
-//     int client_sockets[MAX_CLIENTS];
-//     struct sockaddr_in address;
-// } Server;
-//
-// // Mendeklarasikan fungsi
-// void initialize_server(Server *server);
-// void run_server(Server *server);
-// void send_json_message(int client_socket, const char *message);
-//
-// #endif
-
 #ifndef SERVER_H
 #define SERVER_H
 
 #include <netinet/in.h> // Untuk sockaddr_in
 #include <sys/socket.h> // Untuk socket APIs
 
-#define PORT 8080      // Port yang digunakan server
-#define BACKLOG 10     // Maksimal jumlah klien dalam antrian
-#define BUFFER_SIZE 1024 // Ukuran buffer untuk komunikasi
+#define PORT 8080          // Port yang digunakan server
+#define BACKLOG 10         // Maksimal jumlah klien dalam antrian
+#define BUFFER_SIZE 10240   // Ukuran buffer untuk komunikasi
 
 // Deklarasi fungsi
 void init_server();
 void handle_client(int client_socket);
-void format_message(const char *input, char *output);
+void sanitize_json_string(const char *input, char *output, size_t max_size);
+void format_message(const char *input, char *output, int use_ai);
+int call_ai_api(const char *input, char *response);
 
 #endif
